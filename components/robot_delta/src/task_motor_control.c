4d6611d1a6f3d987db934c516daf83c5caa376a3
Tran Duc Khai <khaixayda@gmail.com>
2026-03-26 00:41:27 +0700

    khởi tạo các hàm hỗ trợ xuất xung PWM

--- components/robot_delta/src/task_motor_control.c
+++ components/robot_delta/src/task_motor_control.c
@@ -1,9 +1,65 @@
 #include "task_motor_control.h"
 
+#include "arm.h"
+#include <math.h>
 
+
+// Các thông số ép xung cho MG90S (Độ phân giải 14-bit)
+// 14-bit có nghĩa là 1 chu kỳ 20ms được chia làm 16384 nấc
+#define DUTY_MIN 410  // Tương đương xung 0.5ms (Góc 0 độ)
+#define DUTY_MAX 1966 // Tương đương xung 2.0ms (Góc 180 độ)
+
+
+static void _Robot_Timer_Init() {
+    // Tần số 50Hz, Độ phân giải 14-bit
+    ledc_timer_config_t ledc_timer = {};
+    ledc_timer.speed_mode       = LEDC_HIGH_SPEED_MODE;
+    ledc_timer.timer_num        = LEDC_TIMER_0; // 3 Arm xài chung 1 Timer
+    ledc_timer.duty_resolution  = LEDC_TIMER_14_BIT;
+    ledc_timer.freq_hz          = 50;           // Tần số chuẩn của MG996R (50Hz)
+    ledc_timer.clk_cfg          = LEDC_AUTO_CLK;
+    ledc_timer_config(&ledc_timer);
+}
+
+// Hàm tính góc đầu ra cho 1 cánh tay
+static float _Robot_Calculate_Servo_Angle(float angle_rad) {
+    // Đổi về Độ (Degrees)
+    float angle_deg = angle_rad * 180.0f / M_PI;
+
+    // Bù góc cơ khí (Đưa góc 0 của IK về tâm 90 của Servo)
+    float servo_angle = 90.0f - angle_deg;
+
+    // Chặn an toàn cơ khí (Chống gãy nhông Servo)
+    if (servo_angle < 0.0f) return 0.0f;
+    if (servo_angle > 180.0f) return 180.0f;
+    
+    return servo_angle;
+}
+
+// ánh xạ Xung (Degree -> Duty)
+static uint32_t _Robot_Calculate_Duty(float servo_angle) {
+    // Ánh xạ tuyến tính từ dải góc (0 - 180) sang dải xung (410 - 1966)
+    return DUTY_MIN + (uint32_t)((servo_angle / 180.0f) * (DUTY_MAX - DUTY_MIN));
+}
+
+// Giao tiếp phần cứng (Duty -> LEDC)
+static void _Robot_Set_PWWM_Duty(arm_object_t *arm, uint32_t duty) {
+    // Đẩy tín hiệu xuống thanh ghi phần cứng ESP32
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, arm->_ledc_channel, duty);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, arm->_ledc_channel);
+}
+
+static void _Robot_Write_PIN(float angle_rad) {
+    float safe_angle = _Robot_Calculate_Servo_Angle(angle_rad);
+    uint32_t duty = _Robot_Calculate_Duty(safe_angle);
+    _Robot_Set_PWWM_Duty(this, duty);
+}
 
 
 void Robot_Motor_Control_Task(void *pvParameters){
+    // Khởi tạo Timer cho việc xuất xung PWM
+    _Robot_Timer_Init();
+
     // Đưa cánh tay về Home
 
 
